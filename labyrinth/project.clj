1470299494cf0bcf2d5568afc49b4fd9fc1bcb2d
Janwillem Swalens <jswalens@vub.ac.be>
2015-05-11 11:47:47 +0200

    Use my fork of Clojure with transactional futures.

--- labyrinth/project.clj
+++ labyrinth/project.clj
@@ -1,7 +1,8 @@
 (defproject labyrinth "0.1.0-SNAPSHOT"
   :description "STAMP Labyrinth in Clojure"
   :url "http://example.com/TODO"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies []
+  :resource-paths ["resources/clojure-1.6.0-transactional-futures.jar"]
   :main ^:skip-aot labyrinth.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
