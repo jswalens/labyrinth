f3be9b2e787638c84104d144816a8c2a6dead33f
Janwillem Swalens <jswalens@vub.ac.be>
2015-05-20 12:41:06 +0200

    Very simple parallel version using pmap.

--- labyrinth/src/labyrinth/router.clj
+++ labyrinth/src/labyrinth/router.clj
@@ -91,7 +91,7 @@
             (if (< n 4)
               #(expand-step-recursive local-grid % dst (inc n) params)
               #(expand-step-iterative local-grid % dst params))]
-      (some true? (map f new-points)))))
+      (some true? (pmap f new-points)))))
 
 (defnp expand [src dst local-grid-initial params]
   "Try to find a path from `src` to `dst` through `local-grid-initial`.
