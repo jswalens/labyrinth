920e9fec155a1f7fb02df6ca574c5b8528fab009
Janwillem Swalens <jswalens@vub.be>
2019-08-05 15:29:39 +0200

    Update to newest Chocola version (v2.0.0), instead of old Clojure fork.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject labyrinth "2.0.0"
   :description "Labyrinth benchmark from STAMP in Clojure, using transactional futures."
   :url "http://soft.vub.ac.be/~jswalens/chocola/"
-  :dependencies [[com.taoensso/timbre "3.4.0"]]
-  :resource-paths ["resources/clojure-1.6.0-transactional-futures-2.3.jar"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.taoensso/timbre "3.4.0"]]
+  :resource-paths ["resources/chocola-2.0.0-standalone.jar"]
+  :injections [(require 'chocola.core)]
   :main ^:skip-aot labyrinth.main
-  :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
