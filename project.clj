400547965e508dd991e5d5998368deadce251c19
Janwillem Swalens <jswalens@vub.be>
2019-08-07 14:34:53 +0200

    Remove dependency on clojure version other than the one in Chocola.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject labyrinth "2.0.0"
   :description "Labyrinth benchmark from STAMP in Clojure, using transactional futures."
   :url "http://soft.vub.ac.be/~jswalens/chocola/"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "3.4.0"]]
+  :dependencies [[com.taoensso/timbre "3.4.0"]]
   :resource-paths ["resources/chocola-2.0.0-standalone.jar"]
   :injections [(require 'chocola.core)]
   :main ^:skip-aot labyrinth.main
